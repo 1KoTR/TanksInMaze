2ae90f3c175a8264bf2b99857342ab214e98e678
1KoTR <denis.kotr@gmail.com>
2021-09-20 23:06:34 +0300

    Научил бота патрулировать локацию

--- Assets/Scripts/Players/BotTankController.cs
+++ Assets/Scripts/Players/BotTankController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,33 +5,53 @@ public class BotTankController : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
 
-    private float _distanceToTarget 
-    {
-        get { return Vector3.Distance(transform.position, _target.position); }
-    }
-    private Transform _target;
+    private GameObject[] _waypoints;
+    private Transform _player;
+    private Transform _currnetWaypoint;
 
-    [SerializeField] private Transform[] _points;
-
-    [SerializeField] private float _viewDistance;
+    [SerializeField] private float _maxDistanceToPlayer;
+    [SerializeField] private float _maxDistanceToWaypoint;
+    [SerializeField] private bool _isPlayerVisiable;
 
     private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
 
-        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        _waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _currnetWaypoint = _waypoints[Random.Range(0, _waypoints.Length - 1)].transform;
     }
 
     private void FixedUpdate()
     {
-        MoveTo();
+        MoveToTarget();
     }
 
-    private void MoveTo()
+    private void MoveToTarget()
     {
-        if (_distanceToTarget <= _viewDistance)
+        if (Vector3.Distance(transform.position, _player.position) <= _maxDistanceToPlayer)
         {
-            _navMeshAgent.SetDestination(_target.position);
+            if (!_isPlayerVisiable)
+                _isPlayerVisiable = true;
+
+            _navMeshAgent.SetDestination(_player.position);
+        }
+        else if (_isPlayerVisiable)
+            _isPlayerVisiable = false;
+
+        if (!_isPlayerVisiable)
+        {
+            if (Vector3.Distance(transform.position, _currnetWaypoint.position) > _maxDistanceToWaypoint)
+            {
+                if (_navMeshAgent.velocity == Vector3.zero)
+                {
+                    _navMeshAgent.SetDestination(_currnetWaypoint.position);
+                }
+            }
+            else if (_navMeshAgent.velocity == Vector3.zero)
+            {
+                _currnetWaypoint = _waypoints[Random.Range(0, _waypoints.Length - 1)].transform;
+            }
         }
     }
 }
